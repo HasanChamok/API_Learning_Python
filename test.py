0a82790d3dade9a10fcd1d5370fa4f36de4ad42e
HasanChamok <hasan.chamok16@gmail.com>
2023-10-09 13:09:53 +0600

    first commit

---
+++ test.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask,jsonify,request
+
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def Home():
+    return "Home"
+
+if __name__ == "__main__":
+    app.run(debug=True)
